0cebf2c6a745c25657986d49a2d7b9793fa56018
kktaro <kk.nnkktr@gmail.com>
2025-03-23 12:03:51 +0900

    Integrate Rearch for state management and define custom app theme

---
+++ lib/ui/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rearch/rearch.dart';
+
+ThemeData appTheme(CapsuleHandle _) => ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+);
